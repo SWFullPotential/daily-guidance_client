5b653325c253374486a305e0f9696ebe650e9903
SWFullPotential <swfullpotential@gmail.com>
2020-10-13 15:38:37 -0600

    created login function and error messages in reducers

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -41,3 +41,33 @@ export const destroySession = () => {
         })
     }
 }
+
+export const createSession = userData => {
+    return dispatch => {
+        const strontParams = {
+            user: {
+                username: userData.username, 
+                password: userData.password
+            }
+        }
+        fetch(URL + 'login', {
+            method: 'POST', 
+            headers: {
+                "Accept": "application/json", 
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify(strongParams)
+        })
+        .then(response => response.json())
+        .then(user => {
+            if(user.errors)
+                return dispatch({type: "CREATE_USER_ERROR", errors: user.errors})
+            else 
+                return didspatch({type: "CREATE_SESSION", user})
+        }) 
+        .catch((errors) => {
+            console.log(errors)
+            dispatch({type: "CREATE_SESSION_ERROR", errors})
+        })
+    }
+}
--- src/reducers/AuthentReducer.js
+++ src/reducers/AuthentReducer.js
@@ -12,6 +12,22 @@ const authentReducer = (state = initialState, action) => {
                 logged_in: true, 
                 user: action.user
             }
+        case 'CREATE_USER_ERROR': 
+            return {
+                ...state, 
+                message: action.errors
+            }
+        case 'CREATE_SESSION':
+            return {
+                ...state, 
+                logged_in: true, 
+                user: action.user
+            }
+        case 'CREATE_SESSION_ERROR': 
+            return {
+                ...state, 
+                message: action.errors
+            }
         case 'DESTROY_SESSION': 
             return {
                 ...state, 
