1e06b4305ac78c2a2c783e8675a007b423862c47
SWFullPotential <swfullpotential@gmail.com>
2020-10-13 16:49:47 -0600

    removed component from import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './Home'
 import Login from './components/authent/Login'
