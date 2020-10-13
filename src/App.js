9333543ba8049f11b8897517b9ef156502e2ec72
SWFullPotential <swfullpotential@gmail.com>
2020-10-13 16:48:54 -0600

    removed logic from App.js that was moved to components and returned App.js to functional component

--- src/App.js
+++ src/App.js
@@ -4,45 +4,8 @@ import Home from './Home'
 import Login from './components/authent/Login'
 import Signup from './components/authent/Signup'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false, 
-      user: {}
-    };
-  };
-  componentDidMount() {
-    this.loginStatus()
-  }
-//should this be moved to my actions? --WILL NEED THUNK MIDDLEWARE
-  loginStatus = () => {
-    fetch('http://localhost:3001/logged_in', {withCredentials: true})
-    .then(response => {
-      if (response.data.logged_in) {
-        this.handleLogin(response)
-      } else {
-        this.handleLogout()
-      }
-    })
-    .catch(error => console.log('api errors:', error))
-  }
-
-  handleLogin = (data) => {
-    this.setState({
-      isLoggedIn: true, 
-      user: data.user
-    })
-  }
-  handleLogout = () => {
-    this.setState({
-      isLoggedIn: false, 
-      user: {}
-    })
-  }
-
-  render() {
-    return (
+function App () {
+      return (
       <div>
         <BrowserRouter>
           <Switch>
@@ -55,5 +18,4 @@ class App extends Component {
       </div>
     );
   }
-};
 export default App;
