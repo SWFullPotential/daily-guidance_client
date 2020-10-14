8c5aca8186ad9cd6d93716552174b07351a702e9
SWFullPotential <swfullpotential@gmail.com>
2020-10-14 11:33:16 -0600

    created different navigation components based on whether or not signed in

---
+++ src/components/layouts/SignedInLinks.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { destroySession } from "../../actions/userActions";
+
+export class SignedInLinks extends Component {
+  logout = () => {
+    this.props.destroySession();
+  };
+  render() {
+    return (
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/" onClick={this.logout}>
+          Logout
+        </NavLink>
+      </nav>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    destroySession: () => dispatch(destroySession()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
---
+++ src/components/layouts/SignedOutLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedOutLinks = () => {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Log-In</NavLink>
+      <NavLink to="/signup">Sign Up</NavLink>
+    </nav>
+  );
+};
+
+export default SignedOutLinks;
