e93d5e01ee55d454861d8cc77d551c79dcc5ef05
SWFullPotential <swfullpotential@gmail.com>
2020-10-14 12:26:42 -0600

    modified destroySession to post instead of get

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,73 +1,77 @@
-const URL = 'http://localhost:3001/'
+const URL = "http://localhost:3001/";
 
 export const createUser = (userData) => {
-    return dispatch => {
-        const strongParams = {
-            user: {
-                username: userData.username, 
-                email: userData.email, 
-                password: userData.password, 
-                password_confirmation: userData.password_confirmation
-            }
-        }
-        fetch(URL + 'users', {
-            method: 'POST', 
-            headers: {
-                "Accept": "application/json", 
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify(strongParams)
-        })
-        .then(response => response.json())
-        .then(user => {
-            if(user.errors)
-                return dispatch({type: "CREATE_USER_ERROR", errors: user.errors})
-                else
-                return dispatch({type: "CREATE_USER", user})
-        })
-        .catch((errors) => {
-            console.log(errors)
-            dispatch({type: "CREATE_USER_ERROR", errors})
-        })
-    }
-}
+  return (dispatch) => {
+    const strongParams = {
+      user: {
+        username: userData.username,
+        email: userData.email,
+        password: userData.password,
+        password_confirmation: userData.password_confirmation,
+      },
+    };
+    fetch(URL + "users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(strongParams),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.errors)
+          return dispatch({ type: "CREATE_USER_ERROR", errors: user.errors });
+        else return dispatch({ type: "CREATE_USER", user });
+      })
+      .catch((errors) => {
+        console.log(errors);
+        dispatch({ type: "CREATE_USER_ERROR", errors });
+      });
+  };
+};
 
 export const destroySession = () => {
-    return dispatch => {
-        fetch(URL + 'logout')
-        .then(response => response.json())
-        .then(logoutData => {
-            dispatch({type: "DESTROY_SESSION", logoutData})
-        })
-    }
-}
+  return (dispatch) => {
+    fetch(URL + "logout", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((logoutData) => {
+        dispatch({ type: "DESTROY_SESSION", logoutData });
+      });
+  };
+};
 
-export const createSession = userData => {
-    return dispatch => {
-        const strongParams = {
-            user: {
-                username: userData.username, 
-                password: userData.password
-            }
-        }
-        fetch(URL + 'login', {
-            method: 'POST', 
-            headers: {
-                "Accept": "application/json", 
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify(strongParams)
-        })
-        .then(response => response.json())
-        .then(user => {
-            if(user.errors)
-                return dispatch({type: "CREATE_USER_ERROR", errors: user.errors})
-            else 
-                return dispatch({type: "CREATE_SESSION", user})
-        }) 
-        .catch((errors) => {
-            console.log(errors)
-            dispatch({type: "CREATE_SESSION_ERROR", errors})
-        })
-    }
-}
+export const createSession = (userData) => {
+  return (dispatch) => {
+    const strongParams = {
+      user: {
+        username: userData.username,
+        password: userData.password,
+      },
+    };
+    fetch(URL + "login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(strongParams),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.errors)
+          return dispatch({ type: "CREATE_USER_ERROR", errors: user.errors });
+        else return dispatch({ type: "CREATE_SESSION", user });
+      })
+      .catch((errors) => {
+        console.log(errors);
+        dispatch({ type: "CREATE_SESSION_ERROR", errors });
+      });
+  };
+};
