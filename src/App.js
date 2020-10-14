9e284268e6845b0489301e72d6633dc89168ecc6
SWFullPotential <swfullpotential@gmail.com>
2020-10-14 11:11:44 -0600

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Home from './Home'
-import Login from './components/authent/Login'
-import Signup from './components/authent/Signup'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/layouts/Home";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 
-function App () {
-      return (
-      <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/login' component={Login}/>
-            <Route exact path='/signup' component={Signup}/>
-
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
 export default App;
