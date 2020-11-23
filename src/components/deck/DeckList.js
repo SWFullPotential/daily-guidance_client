76e18f246478a360c5098fd04eefd2cc26eea0c5
SWFullPotential <swfullpotential@gmail.com>
2020-11-23 16:27:20 -0700

    remove Liker from DeckList

--- src/components/deck/DeckList.js
+++ src/components/deck/DeckList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import Liker from "./Liker";
 class DeckList extends Component {
   constructor() {
     super();
